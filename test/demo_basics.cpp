4f00f863e3cf6445590624b83f48d1df46407c5c
Jan Feitsma <jan.feitsma@asml.com>
2021-03-14 08:54:28 +0100

    add some test coverage for the python command line tools

--- test/demo_basics.cpp
+++ test/demo_basics.cpp
@@ -19,9 +19,17 @@ int main(int argc, char **argv)
     robot_put.pos.x = 1.1;
     robot_put.pos.y = 2.5;
     robot_put.intention = "WIN";
-    std::cout << "writing data into RTDB:" << std::endl;
+    std::cout << "writing struct data into RTDB:" << std::endl;
     std::cout << robot_put << std::endl;
     rtdb->put("ROBOT_DATA", &robot_put);
+    
+    // write some more simple items
+    int i = 37;
+    rtdb->put("TEST_INT", &i);
+    float f = 1.0;
+    rtdb->put("TEST_FLOAT", &f);
+    std::string s = "hi!!";
+    rtdb->put("TEST_STRING", &s);
     
     // sleep a bit
     std::cout << "sleeping a bit ..." << std::endl;
@@ -30,10 +38,9 @@ int main(int argc, char **argv)
     // read a struct
     Robot robot_get;
     rtdb->get("ROBOT_DATA", &robot_get);
-    std::cout << "reading data from RTDB:" << std::endl;
+    std::cout << "reading struct data from RTDB:" << std::endl;
     std::cout << robot_get << std::endl;
 
-    //std::cout << ""
     // TODO: a built-in recursive json formatter would be nice?
     // then we could just dump the entire struct:
     //std::cout << robot_get << std::endl;
