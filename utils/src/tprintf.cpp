8dcc0f27e66fef93fbc270113e8dc7719348b204
Jan Feitsma <jan.feitsma@asml.com>
2021-06-14 20:08:57 +0200

    tprintf: rewrite a little bit, to make it easier to get strings from uftrace logging

--- utils/src/tprintf.cpp
+++ utils/src/tprintf.cpp
@@ -23,4 +23,25 @@ void _tprintf_end()
     fflush(stdout);
 }
 
+void _tprintf_wrap(char const *msg)
+{
+    _tprintf_start();
+    printf("%s", msg);
+    _tprintf_end();
+}
 
+void _tprintf(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    char *tmp = 0;
+    int res = vasprintf(&tmp, fmt, ap);
+    va_end(ap);
+
+    if (res != -1)
+    {
+        _tprintf_wrap(tmp);
+        free(tmp);
+    } // else: vasprintf strangely enough failed; no cleanup nor reporting needed
+}
